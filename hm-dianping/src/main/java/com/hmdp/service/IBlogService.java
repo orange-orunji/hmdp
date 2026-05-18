ad4c7b89f3285794254e3ab91f1313d907a3d146
orunji <3078045959@qq.com>
2026-05-18 20:46:34 +0800

    查询探店笔记

--- hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
@@ -3,6 +3,8 @@ package com.hmdp.service;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -13,4 +15,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 查询最热博客
+     * @param current
+     * @return
+     */
+    List<Blog> queryHotBlogs(Integer current);
+
+    /**
+     * 查询博客详情
+     * @param id
+     * @return
+     */
+    Object queryBlog(Long id);
 }
