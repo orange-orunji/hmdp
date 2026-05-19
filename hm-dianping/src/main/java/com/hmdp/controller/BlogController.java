27f7a1df94a05ea887f30c185058f94a25e468e4
orunji <3078045959@qq.com>
2026-05-19 17:45:28 +0800

    关注滚动动态查询功能业务功能开发

--- hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
@@ -32,13 +32,7 @@ public class BlogController {
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+        return blogService.saveBlog(blog);
     }
 
     @PutMapping("/like/{id}")
@@ -84,6 +78,12 @@ public class BlogController {
         return blogService.queryBlogLikes(id);
     }
 
+    /**
+     * 查询用户博客
+     * @param current
+     * @param id
+     * @return
+     */
     @GetMapping("/of/user")
     public Result queryBlogByUserId(
             @RequestParam(value = "current", defaultValue = "1") Integer current,
@@ -93,5 +93,18 @@ public class BlogController {
                 .eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
         List<Blog> records = page.getRecords();
         return Result.ok(records);
+    }
+
+    /**
+     * 查询用户关注
+     * @param max
+     * @param offset
+     * @return
+     */
+    @GetMapping("/of/follow")
+    public Result queryBlogOfFollow(
+            @RequestParam("lastId") Long max,@RequestParam(name = "offset", defaultValue = "0") Long offset
+    ){
+        return blogService.queryBlogOfFollow(max,offset);
     }
 }
